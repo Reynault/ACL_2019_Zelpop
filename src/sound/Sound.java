fd1b1850963f28dcf37afb67b83d73118221104e
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2019-12-07 21:42:09 +0100

    Fourth sprint: adding sounds

--- src/sound/Sound.java
+++ src/sound/Sound.java
@@ -4,112 +4,55 @@ import java.applet.Applet;
 import java.applet.AudioClip;
 
 public class Sound {
-
-
-
-    public static AudioClip getMonsterAttack() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/monsterAttack.wav")
+    public static AudioClip MONSTER_ATTACK  = Applet.newAudioClip(
+            Sound.class.getResource("/audio/monsterAttack.wav")
         );
-    }
 
-    public static AudioClip getGoblinAttack() {
-        return Applet.newAudioClip(
+    public static AudioClip GOBLIN_ATTACK =  Applet.newAudioClip(
             Sound.class.getResource("/audio/goblinAttack.wav")
         );
-    }
 
+    public static AudioClip HERO_ATTACK_1 =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/heroAttack1.wav")
+    );
 
-    public static AudioClip getHeroAttack1(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/heroAttack1.wav")
-        );
-    }
+    public static AudioClip HERO_ATTACK_2 =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/heroAttack2.wav")
+    );
 
+    public static AudioClip HERO_ATTACK_3 =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/heroAttack3.wav")
+    );
 
-    public static AudioClip getHeroAttack2(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/heroAttack2.wav")
-        );
-    }
+    public static AudioClip GHOST_ATTACK =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/ghostAttack.wav")
+    );
 
+    public static AudioClip MENU_BUTTON =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/buttonMenu.wav")
+    );
 
-    public static AudioClip getHeroAttack3(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/heroAttack3.wav")
-        );
-    }
+    public static AudioClip LOW_LIFE =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/lowLife.wav")
+    );
 
-    public static AudioClip getGhostAttack(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/ghostAttack.wav")
-        );
-    }
+    public static AudioClip STAIRS_SOUND =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/stairs.wav")
+    );
 
-    public static AudioClip getMenuButton(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/buttonMenu.wav")
-        );
-    }
+    public static AudioClip TRAP_SOUND =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/trap.wav")
+    );
 
+    public static AudioClip TELEPORT_SOUND =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/teleport.wav")
+    );
 
-    public static AudioClip getLowLife(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/lowLife.wav")
-        );
-    }
+    public static AudioClip TREASURE_SOUND =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/gold.wav")
+    );
 
-    public static AudioClip getStairs(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/stairs.wav")
-        );
-    }
-
-    public static AudioClip getTrap() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/trap.wav")
-        );
-    }
-
-    public static AudioClip getTeleport() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/teleport.wav")
-        );
-    }
-
-    public static AudioClip getTreasure() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/gold.wav")
-        );
-    }
-
-    public static AudioClip getAmbiance1(){
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/gold.wav")
-        );
-    }
-
-    public static AudioClip getAmbiance2() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/gold.wav")
-        );
-    }
-
-    public static AudioClip getAmbiance3() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/gold.wav")
-        );
-    }
-
-    public static AudioClip getAmbiance4() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/gold.wav")
-        );
-    }
-
-    public static AudioClip getBreak() {
-        return Applet.newAudioClip(
-                Sound.class.getResource("/audio/breakWall.wav")
-        );
-    }
+    public static AudioClip BREAK_SOUND =  Applet.newAudioClip(
+            Sound.class.getResource("/audio/breakWall.wav")
+    );
 }
